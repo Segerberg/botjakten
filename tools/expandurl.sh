61fb62f351ef8743418fbb71a5e9be5a343933a3
peterk <peter@peterkrantz.se>
2018-01-18 09:07:57 +0100

    Expand URL script

---
+++ tools/expandurl.sh
@@ -0,0 +1,6 @@
+# Expand a URL (e.g. link shortener URL) through all redirects and print the
+# URL and final domain name separated by a comma.
+URL=$1
+printf "${URL}, "
+EXPANDURL=$(curl -Ls -o /dev/null -w %{url_effective} ${URL}|awk -F/ '{print $3}')
+printf "${EXPANDURL}\n"
